cadbbf4659ad925488f4f0edd8d2592ee3646651
Irwan M Faizal <irwan279217@gmail.com>
2019-11-07 08:53:40 +0700

    tugas 7 java
    Niomic

---
+++ tugas7java.js
@@ -0,0 +1,12 @@
+var tinggi =[150,165,170,144,156,157,157,159,160,161];
+
+for (var i = 0; i < tinggi.length; i++) {
+  console.log(tinggi[i]);
+}
+
+for (let x of tinggi) {
+  console.log(x);
+}
+
+// forof tidak perlu mengetikan lagi variabel
+// for lebih panjang namun bisa untuk mengeditnya
